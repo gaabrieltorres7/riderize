3aa39ce2179a0a88f2a5ea7764acaee80817ff87
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-27 19:48:02 -0300

    refactor: refresh token

---
+++ src/Http/Controllers/Users/RefreshTokenController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { v4 } from 'uuid';
+import { Validation } from '../../../Utils';
+
+export async function RefreshTokenController(req: Request, res: Response) {
+  const { refreshToken } = req.body;
+  const validation = new Validation(['refreshToken']);
+  const errors = validation.validate({ refreshToken });
+  if(errors.length > 0) { return res.status(400).json({ message: "Validation failed", errors })}
+
+  const newRefreshToken = await generateRefreshToken(req.user.id);
+  const token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET ?? '', { expiresIn: '1d' });
+
+  return res.status(200).json({ message: 'Token refreshed successfully', token, refreshToken: newRefreshToken })
+}
+
+  async function generateRefreshToken(id: number) {
+    const refreshToken = v4();
+    await this.userRepository.updateRefreshToken(id, refreshToken);
+    return refreshToken;
+  }
