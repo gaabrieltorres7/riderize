635fa7cac9e3df76f5f4bca0eff06e6cb04206aa
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-27 16:33:15 -0300

    refactor(users): change controllers classes to a function

--- src/Http/Controllers/Users/CreateUserController.ts
+++ src/Http/Controllers/Users/CreateUserController.ts
@@ -3,31 +3,24 @@ import { Validation } from '../../../Utils';
 import { makeCreateUserUseCase } from '../../../UseCases/Factories/Make-Create-User-UseCase';
 import { UserAlreadyExistsError } from '../../../UseCases/Errors';
 
-export class CreateUserController {
-  private validation: Validation;
+export async function CreateUserController (req: Request, res: Response) {
+  const validation = new Validation(['name', 'email', 'password']);
+  const { name, email, password } = req.body;
+  const errors = validation.validate({ name, email, password });
+  if (errors.length > 0) { return res.status(400).json({ message: 'Validation failed', errors }) };
 
-  constructor() {
-    this.validation = new Validation(['name', 'email', 'password']);
-  }
-
-  async handle(req: Request, res: Response) {
-    const { name, email, password } = req.body;
-    const errors = this.validation.validate({ name, email, password });
-    if (errors.length > 0) { return res.status(400).json({ message: 'Validation failed', errors })};
-
-    try {
-      const createUserUseCase = makeCreateUserUseCase()
-      await createUserUseCase.execute({ name, email, password });
-    } catch (error) {
-      if(error instanceof UserAlreadyExistsError) {
+  try {
+    const createUserUseCase = makeCreateUserUseCase()
+    await createUserUseCase.execute({ name, email, password });
+  } catch (error) {
+    if(error instanceof UserAlreadyExistsError) {
         return res.status(409).json({ message: error.message });
-      }
+    }
 
-      return res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: error.message });
     }
 
     return res.status(201).json({
       message: 'User has been created successfully',
     });
-  }
 }
--- src/Http/Controllers/Users/Routes.ts
+++ src/Http/Controllers/Users/Routes.ts
@@ -8,9 +8,7 @@ export const router = Router();
 
 const prisma = new PrismaClient();
 const userRepository = new PrismaUserRepository(prisma);
-const createUserController = new CreateUserController();
 const getUserController = new GetUserController(userRepository);
-const getAllUsersController = new GetAllUsersController();
 const loginUserController = new LoginUserController(userRepository);
 const getProfileController = new GetProfileController(userRepository);
 const refreshTokenController = new RefreshTokenController(userRepository);
@@ -18,11 +16,11 @@ const refreshTokenController = new RefreshTokenController(userRepository);
 
 router.post("/login", (req, res) => loginUserController.handle(req, res));
 router.post("/refresh-token", (req, res) => refreshTokenController.handle(req, res));
-router.post("/create", (req, res) => createUserController.handle(req, res));
+router.post("/create", (req, res) => CreateUserController(req, res));
 
 router.use(Authentication);
 
-router.get("/all", (req, res) => getAllUsersController.handle(req, res));
+router.get("/all", (req, res) => GetAllUsersController(req, res));
 router.get("/profile", (req, res) => getProfileController.handle(req, res));
 router.get("/:id", (req, res) => getUserController.handle(req, res));
 
