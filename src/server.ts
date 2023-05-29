b6c5d97215203bdbd205763b48b758ae0ee47291
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-29 20:43:32 -0300

    refactor: handler errors in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-// import "express-async-errors"
+import express, { NextFunction, Request, Response } from 'express';
+import "express-async-errors"
 import UserRoute from './Http/Controllers/Users/Routes';
 import PedalRoute from './Http/Controllers/Pedals/Routes';
 import SubscriptionRoute from './Http/Controllers/Subscriptions/Routes';
-// import { AppError } from './Errors/AppError';
+import { ResourceNotFoundError, ParticipantsLimitReachedError, RegistrationPeriodEndedError, UserAlreadyExistsError, UserAlreadySubscribedError } from './UseCases/Errors';
 
 require('dotenv').config();
 
@@ -17,14 +17,22 @@ app.use("/pedals", PedalRoute);
 app.use("/subscriptions", SubscriptionRoute);
 
 // AppError middleware + lib express-async-errors
-// app.use(
-//   (err: Error, req: Request, res: Response, next: NextFunction) => {
-//   if(err instanceof AppError) {
-//     return res.status(err.statusCode).json({message: err.message});
-//   }
-//   console.log(err)
-//   return res.status(500).json({message: "Internal server error"});
-// });
+app.use(
+  (err: Error, req: Request, res: Response, next: NextFunction) => {
+  if(err instanceof ResourceNotFoundError) {
+    return res.status(404).json({message: err.message});
+  }else if(err instanceof UserAlreadyExistsError) {
+    return res.status(400).json({message: err.message});
+  }else if(err instanceof UserAlreadySubscribedError) {
+    return res.status(409).json({message: err.message});
+  }else if(err instanceof RegistrationPeriodEndedError) {
+    return res.status(409).json({message: err.message});
+  }else if(err instanceof ParticipantsLimitReachedError) {
+    return res.status(409).json({message: err.message});
+  }
+  console.log(err)
+  return res.status(500).json({message: "Internal server error"});
+});
 
 app.listen(port, () => {
   console.log('Server is running');
