81de586e437691d327f16603b4e147167e518ce1
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-29 21:38:42 -0300

    fix: subscribe/:ride_id

--- src/Http/Controllers/Subscriptions/Routes.ts
+++ src/Http/Controllers/Subscriptions/Routes.ts
@@ -6,7 +6,7 @@ export const router = Router();
 
 router.use(Authentication);
 
-router.post("/subscribe", (req, res) => SubscriptionController(req, res));
+router.post("/subscribe/:ride_id", (req, res) => SubscriptionController(req, res));
 router.get("/listParticipation", (req, res) => ListPedalsUserParticipatedController(req, res));
 
 export default router;
