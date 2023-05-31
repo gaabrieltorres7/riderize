6a894e6394712b9bab082e17dc10f48fd043dfe2
Gabriel Torres <gaabrieltt7@gmail.com>
2023-05-31 12:15:04 -0300

    refactor: error middleware

--- src/server.ts
+++ src/server.ts
@@ -17,25 +17,24 @@ app.use("/pedals", PedalRoute);
 app.use("/subscriptions", SubscriptionRoute);
 
 // AppError middleware + lib express-async-errors
-app.use(
-  (err: Error, req: Request, res: Response, next: NextFunction) => {
-  if(err instanceof ResourceNotFoundError) {
-    return res.status(404).json({message: err.message});
-  }else if(err instanceof UserAlreadyExistsError) {
-    return res.status(400).json({message: err.message});
-  }else if(err instanceof UserAlreadySubscribedError) {
-    return res.status(409).json({message: err.message});
-  }else if(err instanceof RegistrationPeriodEndedError) {
-    return res.status(409).json({message: err.message});
-  }else if(err instanceof ParticipantsLimitReachedError) {
-    return res.status(409).json({message: err.message});
-  }else if(err instanceof InvalidCredentialsError) {
-    return res.status(400).json({message: err.message});
-  }else if(err instanceof PedalNameAlreadyExistsError) {
-    return res.status(409).json({message: err.message});
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  let statusCode = 500;
+  let errorMessage = "Internal server error";
+
+  if (err instanceof ResourceNotFoundError) {
+    statusCode = 404;
+  } else if (err instanceof UserAlreadyExistsError || err instanceof InvalidCredentialsError) {
+    statusCode = 400;
+  } else if (err instanceof UserAlreadySubscribedError || err instanceof RegistrationPeriodEndedError || err instanceof ParticipantsLimitReachedError || err instanceof PedalNameAlreadyExistsError) {
+    statusCode = 409;
   }
-  console.log(err)
-  return res.status(500).json({message: "Internal server error"});
+
+  if (statusCode !== 500) {
+    errorMessage = err.message;
+  }
+
+  console.log(err);
+  return res.status(statusCode).json({ message: errorMessage });
 });
 
 app.listen(port, () => {
