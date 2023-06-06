e5a57dac3e7d4e77f19bc21a2c2625ff215975e3
Gabriel Torres <gaabrieltt7@gmail.com>
2023-06-06 16:13:04 -0300

    test: should be able to show pedals with pagination

--- src/UseCases/ShowPedals.spec.ts
+++ src/UseCases/ShowPedals.spec.ts
@@ -40,4 +40,31 @@ describe('Show pedals useCase', () => {
     const { pedals } = await sut.execute({})
     expect(pedals[0].name).toBe('Teste1')
   })
+
+  it('should be able to show pedals with pagination', async () => {
+    const user = await usersRepository.create({
+      name: 'any_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+    })
+
+    for (let i = 1; i <= 5; i++) {
+      await pedalRepository.create({
+        id: 1 + i,
+        name: `Teste${i}`,
+        start_date: new Date(2020, 1, 1),
+        start_date_registration: new Date(2020, 1, 1),
+        end_date_registration: new Date(2020, 1, 1),
+        additional_information: 'Teste',
+        start_place: 'Teste',
+        participants_limit: 10,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        authorId: user.id,
+      })
+    }
+
+    const { pedals } = await sut.execute({ skip: 0, take: 2 })
+    expect(pedals[1].name).toBe('Teste2')
+  })
 })
