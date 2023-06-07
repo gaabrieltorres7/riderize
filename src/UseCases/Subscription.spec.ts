3aafa79d3ef700e6a62bd4d1ad50caa9bd7c14f2
Gabriel Torres <gaabrieltt7@gmail.com>
2023-06-07 09:20:01 -0300

    test: should not be able to subscribe on pedal user has already subscribed

--- src/UseCases/Subscription.spec.ts
+++ src/UseCases/Subscription.spec.ts
@@ -3,12 +3,7 @@ import { InMemoryUserRepository } from '../Repositories/In-Memory/In-Memory-User
 import { InMemoryPedalRepository } from '../Repositories/In-Memory/In-Memory-PedalRepository'
 import { InMemorySubscriptionsRepository } from '../Repositories/In-Memory/In-Memory-SubscriptionsRepository'
 import { SubscriptionUseCase } from './SubscriptionUseCase'
-// import {
-//   ParticipantsLimitReachedError,
-//   RegistrationPeriodEndedError,
-//   ResourceNotFoundError,
-//   UserAlreadySubscribedError,
-// } from './Errors'
+import { UserAlreadySubscribedError } from './Errors'
 
 let usersRepository: InMemoryUserRepository
 let pedalRepository: InMemoryPedalRepository
@@ -51,5 +46,39 @@ describe('Subscription on pedals useCase', () => {
 
     expect(response.subscription.ride_id).toBe(1)
     expect(response.subscription.user_id).toBe(1)
+  })
+
+  it('should not be able to subscribe on pedal user has already subscribed', async () => {
+    const user = await usersRepository.create({
+      name: 'any_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+    })
+
+    const pedal = await pedalRepository.create({
+      id: 1,
+      name: 'Teste',
+      start_date: new Date(2023, 9, 9),
+      start_date_registration: new Date(2023, 6, 6),
+      end_date_registration: new Date(2023, 8, 9),
+      additional_information: 'Teste',
+      start_place: 'Teste',
+      participants_limit: 10,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      authorId: user.id,
+    })
+
+    await sut.execute({
+      ride_id: pedal.id,
+      user_id: user.id,
+    })
+
+    await expect(async () => {
+      await sut.execute({
+        ride_id: pedal.id,
+        user_id: user.id,
+      })
+    }).rejects.toBeInstanceOf(UserAlreadySubscribedError)
   })
 })
